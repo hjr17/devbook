b79a8adc6eb0516cdf5baa01e643a5bf7fcdf87b
hjr17 <hamiltonjrribeiro@hotmail.com>
2025-03-10 16:50:05 -0300

    Sending files from the DevBook API of the Golang course (Udemy)

---
+++ api/src/autentication/token.go
@@ -0,0 +1,18 @@
+package autenticacao
+
+import (
+	"api/src/config"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+// CriarToken retorna um token assinado com as permissões
+func CriarToken(usuarioID uint64) (string, error) {
+	permissoes := jwt.MapClaims{}
+	permissoes["authorized"] = true
+	permissoes["exp"] = time.Now().Add(time.Hour * 6).Unix() // 13235586
+	permissoes["usuarioId"] = usuarioID
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, permissoes)
+	return token.SignedString([]byte(config.SecretKey)) // secret
+}
