13b83165432199a6f631435ba4abec8b1408ee7e
hjr17 <hamiltonjrribeiro@hotmail.com>
2025-03-19 14:30:12 -0300

    Implemented: Communicating with the API; Responses package; Authenticating to the API via the login route; Modifying the API login response; Obtaining a new response; Config package.

--- api/main.go
+++ api/main.go
@@ -24,6 +24,6 @@ func main() {
 
 	/* fmt.Println(config.SecretKey) */
 
-	fmt.Printf("Escutando na porta %d", config.Porta)
+	fmt.Printf("Escutando na porta %d\n", config.Porta)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 }
